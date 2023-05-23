4079ed1887a2a1744f3485af4adcc30a34be614a
Alan del Fresno <alandelfresno2@gmail.com>
2023-05-23 14:34:22 -0300

    Restructure HTML template into Angular

---
+++ src/app/components/counter-up/counter-up.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-up',
+  templateUrl: './counter-up.component.html',
+  styleUrls: ['./counter-up.component.scss']
+})
+export class CounterUpComponent {
+
+}
---
+++ src/app/components/media-grid/media-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-grid',
+  templateUrl: './media-grid.component.html',
+  styleUrls: ['./media-grid.component.scss']
+})
+export class MediaGridComponent {
+
+}
