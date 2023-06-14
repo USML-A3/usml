a6a7f4ef3a8d8d40e4f065a13f840e28b2a04793
Alan del Fresno <alandelfresno2@gmail.com>
2023-06-14 12:01:04 -0300

    Media grid loop rendering

--- src/shared/components/media-grid/media-grid.component.ts
+++ src/shared/components/media-grid/media-grid.component.ts
@@ -3,8 +3,64 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-media-grid',
   templateUrl: './media-grid.component.html',
-  styleUrls: ['./media-grid.component.scss']
+  styleUrls: ['./media-grid.component.scss'],
 })
 export class MediaGridComponent {
-
+  mediaGridData: GridCardData[] = [
+    {
+      imgName: 'Kivu 1 square.jpg',
+      title: 'USMLOPS Kivu',
+      spanText: "N'Ziwasogo",
+    },
+    {
+      imgName: 'Kivu 2 square.jpg',
+      title: 'USMLOPS Kivu',
+      spanText: "N'Ziwasogo",
+    },
+    {
+      imgName: 'Escudo de Paz 1 square.jpg',
+      title: 'Operacion Escudo de Paz',
+      spanText: 'Misión no oficial',
+    },
+    {
+      imgName: 'Escudo de Paz 2 square.jpg',
+      title: 'Operacion Escudo de Paz',
+      spanText: 'Misión no oficial',
+    },
+    {
+      imgName: 'Napf 1 square.png',
+      title: 'USMLOPS',
+      spanText: 'Napf',
+    },
+    {
+      imgName: 'Napf 2 square.jpg',
+      title: 'USMLOPS',
+      spanText: 'Napf',
+    },
+    {
+      imgName: 'Napf 4 square.jpg',
+      title: 'USMLOPS',
+      spanText: 'Napf',
+    },
+    {
+      imgName: 'Rosario 1 square.jpg',
+      title: 'Operación Rosario',
+      spanText: 'Misión no oficial',
+    },
+    {
+      imgName: 'Rosario 2 square.jpg',
+      title: 'Operación Rosario',
+      spanText: 'Misión no oficial',
+    },
+  ];
 }
+interface GridCardData {
+  imgName: string;
+  title: string;
+  spanText: string;
+}
+// type GridCardData = Array<{
+//   imgName: string;
+//   title: string;
+//   spanText: string;
+// }>;
